13f0af892e5e7ed02c273986571295205d11d2d6
dilbarov03 <dilbarov03@gmail.com>
2024-09-21 20:35:41 +0500

    change deploy settins

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -2,3 +2,9 @@ from .base import *  # noqa
 
 DEBUG = True
 CELERY_TASK_ALWAYS_EAGER = True
+
+
+CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = [
+    "https://satyr-helped-intensely.ngrok-free.app/"
+]
